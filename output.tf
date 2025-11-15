8655db20cda9b3fb9eddfa5656f2c2bdc868d290
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-15 15:28:54 +0530

    changes

--- output.tf
+++ output.tf
@@ -1,10 +1,16 @@
+
 output "vpc_id" {
-    value = aws_vpc.main.id
-  
+  value       = aws_vpc.main.id
 }
 
-output "azs"{
-    value = data.aws_availability_zones.available.names
-
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnet[*].id
 }
 
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnet[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database_subnet[*].id
+}
